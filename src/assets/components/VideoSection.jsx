d24214660ce4698518af32330f479dd97917e8dd
HusnainDot <husnainmaroof95@gmail.com>
2025-06-25 20:57:01 +0500

    changes

--- src/assets/components/VideoSection.jsx
+++ src/assets/components/VideoSection.jsx
@@ -1,8 +1,10 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import video from "../images/video.mp4";
 import gsap from "gsap";
 
 const VideoSection = () => {
+  const [onmouse, setonmouse] = useState(false);
+
   const mouse = useRef(null);
 
   const mouseEnter = () => {
@@ -14,8 +16,8 @@ const VideoSection = () => {
 
   const mouseMove = (dets) => {
     gsap.to(mouse.current, {
-      left: dets.clientX - 50,
-      top: dets.clientY - 50,
+      left: dets.clientX - 20,
+      top: dets.clientY - 20,
       duration: 0.3,
       ease: "power2.out",
     });
@@ -30,22 +32,48 @@ const VideoSection = () => {
 
   return (
     <div className="px-5 w-full my-5 relative">
-      <video
-        onMouseEnter={mouseEnter}
-        onMouseMove={mouseMove}
-        onMouseLeave={mouseLeave}
-        src={video}
-        className="w-auto h-auto object-cover rounded-md md:rounded-2xl hover:cursor-pointer"
-        autoPlay
-        muted
-        loop
-      />
+      <div>
+        <video
+          onMouseEnter={mouseEnter}
+          onMouseMove={mouseMove}
+          onMouseLeave={mouseLeave}
+          src={video}
+          className="w-auto h-auto object-cover rounded-md md:rounded-2xl hover:cursor-pointer"
+          autoPlay
+          muted
+          loop
+        />
 
-      <div
-        ref={mouse}
-        className="w-[100px] h-[100px] top-0 left-0 bg-black z-10 absolute rounded-full opacity-0 scale-0 flex items-center justify-center font-semibold text-2xl text-white "
-      >
-        play
+        <div
+          ref={mouse}
+          className="w-[100px] h-[100px] top-0 left-0 bg-black z-10 absolute rounded-full opacity-0 scale-0 flex items-center justify-center font-semibold text-2xl text-white "
+        >
+          play
+        </div>
+      </div>
+
+      <div className="my-10 md:my-20">
+        <div className="flex flex-col ">
+          <h3 className="font-bold text-3xl md:text-6xl text-center py-2 md:py-5">
+            TAKE THE PLEDGE.
+          </h3>
+          <p className="font-semibold text-sm md:text-lg text-center ">
+            Support and empower women experiencing domestic violence, every day
+            of the year..
+          </p>
+
+          <div className="flex flex-col items-center justify-center w-fit mx-auto pt-5 ">
+            <a
+
+              href="#"
+              className="text-md uppercase font-mono  text-center cursor-pointer"
+            >
+              learn more today
+            </a>
+
+            <div className={`bg-gray-500 w-full h-[1px]  transition-all duration-200 hover: `}></div>
+          </div>
+        </div>
       </div>
     </div>
   );
